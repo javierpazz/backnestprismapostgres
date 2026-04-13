b788310e835959ea5d4f2159caac165ceb45e6ef
JAVIER <javierpia0105@gmail.com>
2026-04-13 19:38:32 -0300

    deploy

---
+++ public/assets/FullScreenLoading-7502a769.js
@@ -0,0 +1,1 @@
+import{j as e,B as s,a as n,w as r}from"./mui-eee1b04c.js";const a=()=>e.jsxs(s,{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",height:"calc(100vh - 200px)",children:[e.jsx(n,{sx:{mb:3},variant:"h2",fontWeight:200,fontSize:20,children:"Cargando..."}),e.jsx(r,{thickness:2})]});export{a as F};
---
+++ public/assets/Header-26ec2292.js
@@ -0,0 +1,1 @@
+import{j as e}from"./mui-eee1b04c.js";const r=()=>e.jsx("header",{className:"flex flex-col items-center justify-center mb-5 xl:flex-row xl:justify-between"});export{r as H};
